81b8a7f528358d1cb30a74b60f57018d98e9e5bd
Marcos Sokolowski <Marcos@MacBook-Pro-de-Marcos.local>
2014-01-27 15:07:22 -0200

    -

---
+++ Location/LocationTest/LocationTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LocationTest
+//
+//  Created by Marcos Sokolowski on 23/01/14.
+//  Copyright (c) 2014 Marcos Sokolowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
