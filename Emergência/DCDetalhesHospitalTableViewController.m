01565d94803dc09a518d6dad4f5016c7cfb495fd
Joao Pedro da Costa Nunes <jo40zinhux@JoaoPedro.local>
2014-05-23 16:32:34 -0300

    TableView com detalhes de cada hospital

--- Emergência/DCDetalhesHospitalTableViewController.m
+++ Emergência/DCDetalhesHospitalTableViewController.m
@@ -25,16 +25,10 @@
 
 - (void)viewDidLoad
 {
-    //_postos = [[DCPosto alloc]init];
     [super viewDidLoad];
     
-    NSLog(@" nome%@",self.postos.nome);
-    
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    UIImageView *backgroundView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"background.png"]];
+    self.tableView.backgroundView = backgroundView;
 }
 
 - (void)didReceiveMemoryWarning
@@ -43,20 +37,16 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*-(void) addPosto {
- [_detalhePosto addObject:_postos];
- }*/
-
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-    return 3;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 2;
+    return 1;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -67,22 +57,47 @@
     if(cell == nil) {
         cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:tableIdent];
     }
+    UILabel *lblEndereco = (UILabel *)[cell viewWithTag:1];
+    //UIButton *btAceita = (UIButton *) [cell viewWithTag:1];
     
-    UILabel *lblPosto = (UILabel *)[cell viewWithTag:1];
-    lblPosto.text = _postos.telefone;
-    cell.tag = indexPath.row+1;
+    lblEndereco.lineBreakMode = YES;
+    lblEndereco.lineBreakMode = NSLineBreakByCharWrapping;
+    lblEndereco.text = _postos.endereco;
+    lblEndereco.textColor = [UIColor whiteColor];
+    lblEndereco.frame = CGRectMake(0, 0, cell.frame.size.width, cell.frame.size.height);
+
+    //[btAceita setImage:[UIImage imageNamed:@"hospital.png"] forState:UIControlStateNormal];
+    //[btAceita addTarget:self action:@selector(aceitarHospital) forControlEvents:UIControlEventTouchUpInside];
+    
+    cell.tag = indexPath.row;
+    cell.backgroundColor = [UIColor clearColor];
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    
     return cell;
 }
 
--(NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+-(void) aceitarHospital {
+    NSLog(@"FUNCIONOU");
+}
+
+-(UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
     
-    if (section == 0) {
-        return _postos.nome;
-    } else if (section == 1) {
-        return _postos.endereco;
-    } else {
-        return  _postos.telefone;
-    }
+    UIView *headerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, tableView.bounds.size.width, tableView.bounds.size.height)];
+    UILabel *lblHeader = [[UILabel alloc] initWithFrame:CGRectMake(0.0, 0.5, 400, 20)];
+    
+    lblHeader.text = _postos.nome;
+    lblHeader.lineBreakMode = YES;
+    lblHeader.lineBreakMode = NSLineBreakByCharWrapping;
+    lblHeader.textColor = [UIColor colorWithRed:(107/255.0) green:0 blue:(2/255.0) alpha:1];
+    lblHeader.font = [UIFont boldSystemFontOfSize:1.0f];
+    [lblHeader setFont:[UIFont systemFontOfSize:13.0f]];
+    
+    headerView.backgroundColor = [UIColor whiteColor];
+    [lblHeader setBackgroundColor: [UIColor clearColor]];
+    
+    [headerView addSubview:lblHeader];
+    
+    return headerView;
 }
 
 /*
